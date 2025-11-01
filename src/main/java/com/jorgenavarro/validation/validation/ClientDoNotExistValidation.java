7dcd9f2cb6a0ebcebe56e9e983677c4085353ad2
Jorge Navarro <130685403+JorgeNavarro-Programmer@users.noreply.github.com>
2025-11-01 14:41:05 +0100

    Code was documented

--- src/main/java/com/jorgenavarro/validation/validation/ClientDoNotExistValidation.java
+++ src/main/java/com/jorgenavarro/validation/validation/ClientDoNotExistValidation.java
@@ -7,14 +7,34 @@ import com.jorgenavarro.validation.validation.annotions.ClientDoNotExist;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * @author Jorge Navarro
+ *         This class valid if the client don´t exist
+ */
 public class ClientDoNotExistValidation implements ConstraintValidator<ClientDoNotExist, Client> {
 
+    /**
+     * Used to vilid if the client exist
+     */
     private ClientService clientService;
 
+    /**
+     * Constructor the ClientDoNotExistValidation
+     * 
+     * @param clientService - to find the user
+     */
     public ClientDoNotExistValidation(ClientService clientService) {
         this.clientService = clientService;
     }
 
+    /**
+     * Comprobe if the user exist or not
+     * 
+     * @param value   - client to valid to exist or not
+     * @param context
+     * 
+     * @return true if the user don´t exist, false if the user exist
+     */
     @Override
     public boolean isValid(Client value, ConstraintValidatorContext context) {
         if (clientService.findByUsername(value.getUserName()) == null) {
--- src/main/java/com/jorgenavarro/validation/validation/ClientNotFoundValidation.java
+++ src/main/java/com/jorgenavarro/validation/validation/ClientNotFoundValidation.java
@@ -6,17 +6,35 @@ import com.jorgenavarro.validation.validation.annotions.ClientNotFound;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * @author Jorge Navarro
+ *         This class valid if the client exist
+ */
 public class ClientNotFoundValidation implements ConstraintValidator<ClientNotFound, Long> {
 
+    /**
+     * Used to find the user
+     */
     private ClientService clientService;
 
+    /**
+     * Constructor the ClientNotFoundValidation
+     * 
+     * @param clientService - to find the user
+     */
     public ClientNotFoundValidation(ClientService clientService) {
         this.clientService = clientService;
     }
 
+    /**
+     * This method valid the input id
+     * 
+     * @param value - Client id
+     * @return true if the client is found - false if the client is not found
+     */
     @Override
     public boolean isValid(Long value, ConstraintValidatorContext context) {
-        if(clientService.findById(value) == null){
+        if (clientService.findById(value) == null) {
             return false;
         }
         return true;
